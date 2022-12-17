3a2c01bdd9b4662b89192fac81c0a4637a2a128c
suman chapai <sumanchapai@gmail.com>
2022-12-17 12:20:46 -0600

    Cache bad nights list for performace reasons

--- trout/nights/__init__.py
+++ trout/nights/__init__.py
@@ -1,10 +1,13 @@
 from trout.database import query
 
+@cache
 def bad_nights(limit:int=0):
     """
     Get the list of bad nights from the database.
+    Note that this function is cached which means that if you have recently made
+    changes to the list of bad_nights, then you might have to be careful.
 
-    param limit(optional): number of results to limit to
+    param limit(optional): number of results to limit to. Defaults to no limit.
     return: List of 2 tuple (id, date)
     """
     if limit > 0: 
