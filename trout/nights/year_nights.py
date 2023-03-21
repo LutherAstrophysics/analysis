216ec995063f4e2e83073b64bf7ff8668b316c74
suman chapai <sumanchapai@gmail.com>
2023-03-21 11:35:05 -0500

    Bugfix

--- trout/nights/year_nights.py
+++ trout/nights/year_nights.py
@@ -6,14 +6,17 @@ from trout.stars import get_star
 
 
 @cache
-def get_nights_in_a_year(year: int) -> Iterable[date]:
+def get_nights_in_a_year(year: int, is_primary: bool = True) -> Iterable[date]:
     """
     Returns the list of all night dates for a given `year`
     Note that we get the list of dates by just looking at the nights
-    on which star 1 had data. 
+    on which star 1 had data.
     """
-    star_one = get_star(1)
+    star_one = get_star(1, is_primary)
     # Select data for the given year
     # Note that we're not excluding bad nights and zero values
-    star_one.select(f"date >= '{year}-01-01' and date< '{year + 1}-01-01'", exclude_bad_nights=False, exclude_zeros=False)
+    star_one.select(
+        f"date >= '{year}-01-01' and date< '{year + 1}-01-01'",
+        exclude_bad_nights=False,
+        exclude_zeros=False)
     return star_one.get_selected_dates_column()
