1c2e6baacb10cab8896c3f9ca9f8969288f658ab
suman chapai <sumanchapai@gmail.com>
2023-05-22 14:50:03 -0500

    Revert "Enhance exports"
    This reverts commit da8430048ea69fd3348dcb56a391883e4cf4ab7f.

--- trout/files/__init__.py
+++ trout/files/__init__.py
@@ -1,7 +1,5 @@
 import re
 
-from .reference_log_file import ReferenceLogFile
-
 # If we had added m23 as a dependency we wouldn't need to add this file module
 # as it's already implemented in the m23 library. But m23 library is too big and
 # perhaps unnecessary to add as a dependency.
@@ -13,6 +11,3 @@ def line_str_contains_numbers_and_non_alphabets(line_str: str) -> bool:
     characters
     """
     return re.search(r"\d+", line_str) and not re.search("[a-zA-Z]", line_str)
-
-
-__all__ = ["line_str_contains_numbers_and_non_alphabets", "ReferenceLogFile"]
